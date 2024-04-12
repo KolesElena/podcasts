861c119d16f6b9b2a0e0a20eedf7b2cd7e005bea
Elena Kolesnikova <helena_uni@live.ru>
2024-04-12 18:19:01 +0200

    implemented routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
 import './index.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PodcastsList from './pages/PodcastsList/PodcastsList';
+import PodcastsDetails from './pages/PodcastDetails/PodcastDetails';
 
 const queryClient = new QueryClient();
 
@@ -13,7 +17,12 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PodcastsList />} />
+          <Route path="/podcasts/:podcastId" element={<PodcastsDetails />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 );
