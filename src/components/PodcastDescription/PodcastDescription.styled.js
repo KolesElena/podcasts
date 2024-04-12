6f29349763c9ac3c5eec135b0c73fd4b827ee4e8
Elena Kolesnikova <helena_uni@live.ru>
2024-04-12 18:21:24 +0200

    implemented PodcastDescription component

---
+++ src/components/PodcastDescription/PodcastDescription.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const StyledPodcastDescription = styled.div`
+  display: block;
+  width: 200px;
+  min-width: 200px;
+  height: 250px;
+  margin: 0 30px;
+  border-radius: 4px;
+  box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12);
+`;
+
+export const StyledImage = styled.div`
+  margin-top: 10px;
+  width: 100%;
+`;
+
+export const StyledText = styled.div`
+  margin: 10px; 
+`;
