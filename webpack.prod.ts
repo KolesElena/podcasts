8db46565d4dfde467d05279524eaf809d39002ee
Elena Kolesnikova <helena_uni@live.ru>
2024-04-18 00:05:33 +0200

    fixed prod webpack configuration

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,4 +1,5 @@
 const TerserPlugin = require("terser-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 const { merge } = require('webpack-merge');
  const common = require('./webpack.common.ts');
@@ -6,8 +7,20 @@ const { merge } = require('webpack-merge');
 
  module.exports = merge(common, {
    mode: 'production',
+   performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
    optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [
+      new TerserPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+        },
+      }),
+    ],
   },
  });
